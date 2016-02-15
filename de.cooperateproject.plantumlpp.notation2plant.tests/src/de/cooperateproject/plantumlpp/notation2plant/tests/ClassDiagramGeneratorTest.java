869d3bfbbcde6694a45b1e12c7eecb7294e59102
thomcz <thomcz@users.noreply.github.com>
2016-02-15 09:49:00 +0100

    #7 removed '\r' from result strings to be compatible with unix

--- de.cooperateproject.plantumlpp.notation2plant.tests/src/de/cooperateproject/plantumlpp/notation2plant/tests/ClassDiagramGeneratorTest.java
+++ de.cooperateproject.plantumlpp.notation2plant.tests/src/de/cooperateproject/plantumlpp/notation2plant/tests/ClassDiagramGeneratorTest.java
@@ -53,228 +53,228 @@ public class ClassDiagramGeneratorTest extends FileRessource {
 	public void ClassAttributOperationTest() {
 		Diagram d = getDiagram(diagrams, "ClassAttrOp");
 		
-		assertEquals("@startuml\r\ntitle ClassAttrOp\r\n"
-				+ "class A {\r\n\t+Attribute1: String\r\n\t"
-				+ "+Attribute2: String[]\r\n\t+Operation1()\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle ClassAttrOp\n"
+				+ "class A {\n\t+Attribute1: String\n\t"
+				+ "+Attribute2: String[]\n\t+Operation1()\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void InterfaceTest() {
 		Diagram d = getDiagram(diagrams, "Interface");
 		
-		assertEquals("@startuml\r\ntitle Interface\r\n"
-				+ "interface Interface {\r\n\t"
-				+ "+Attribute1\r\n\t+Operation1()\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Interface\n"
+				+ "interface Interface {\n\t"
+				+ "+Attribute1\n\t+Operation1()\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void EmptyPackageTest() {
 		Diagram d = getDiagram(diagrams, "EmptyPackage");
 		
-		assertEquals("@startuml\r\ntitle EmptyPackage\r\n"
-				+ "package Package {\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle EmptyPackage\n"
+				+ "package Package {\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void PackageWithClassTest() {
 		Diagram d = getDiagram(diagrams, "PackageClass");
 		
-		assertEquals("@startuml\r\ntitle PackageClass\r\n"
-				+ "package Package {\r\n\t"
-				+ "class Class {\r\n\t}\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle PackageClass\n"
+				+ "package Package {\n\t"
+				+ "class Class {\n\t}\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void EnumerationTest() {
 		Diagram d = getDiagram(diagrams, "Enumeration");		
 		
-		assertEquals("@startuml\r\ntitle Enumeration\r\n"
-				+ "enum Enumeration {\r\n\tEnumerationLiteral1\r\n\t"
-				+ "EnumerationLiteral2\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Enumeration\n"
+				+ "enum Enumeration {\n\tEnumerationLiteral1\n\t"
+				+ "EnumerationLiteral2\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void DataTypeTest() {
 		Diagram d = getDiagram(diagrams, "DataType");
 		
-		assertEquals("@startuml\r\ntitle DataType\r\n"
-				+ "class DataType << (D,blue) >> {\r\n\t"
-				+ "+Attribute1\r\n\t+Operation1()\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle DataType\n"
+				+ "class DataType << (D,blue) >> {\n\t"
+				+ "+Attribute1\n\t+Operation1()\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void SignalTest() {
 		Diagram d = getDiagram(diagrams, "Signal");
 				
-		assertEquals("@startuml\r\ntitle Signal\r\n"
-				+ "class Signal << (S,red) >> {\r\n\t"
-				+ "+Attribute1\r\n\t+Attribute2\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Signal\n"
+				+ "class Signal << (S,red) >> {\n\t"
+				+ "+Attribute1\n\t+Attribute2\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void InformationItemTest() {
 		Diagram d = getDiagram(diagrams, "InformationItem");
 				
-		assertEquals("@startuml\r\ntitle InformationItem\r\n"
-				+ "class InformationItem << (>,orchid) >> {\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle InformationItem\n"
+				+ "class InformationItem << (>,orchid) >> {\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void InstanceSpecificationTest() {
 		Diagram d = getDiagram(diagrams, "InstanceSpecification");
 				
-		assertEquals("@startuml\r\ntitle InstanceSpecification\r\n"
-				+ "class \"InstanceSpecification:\" << (i,pink) >> {\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle InstanceSpecification\n"
+				+ "class \"InstanceSpecification:\" << (i,pink) >> {\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void CommentTest() {
 		Diagram d = getDiagram(diagrams, "Comment");
 		
-		assertEquals("@startuml\r\ntitle Comment\r\nclass Class {\r\n}\r\n"
-				+ "note as Ncom\r\n\tcomment\r\nend note\r\n\tNcom..Class\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Comment\nclass Class {\n}\n"
+				+ "note as Ncom\n\tcomment\nend note\n\tNcom..Class\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void DurationObservationTest() {
 		Diagram d = getDiagram(diagrams, "DurationObservation");
 				
-		assertEquals("@startuml\r\ntitle DurationObservation\r\n"
-				+ "note as NDurationDurationObservation\r\n\t\r\nend note\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle DurationObservation\n"
+				+ "note as NDurationDurationObservation\n\t\nend note\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void DurationObservationWithEventTest() {
 		Diagram d = getDiagram(diagrams, "DurationObservationWithEvent");
 
-		assertEquals("@startuml\r\ntitle DurationObservationWithEvent\r\n"
-				+ "note as NDurationDurationObservation\r\n\t\r\nend note\r\n\t"
-				+ "NDurationDurationObservation..A\r\n"
-				+ "class A {\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle DurationObservationWithEvent\n"
+				+ "note as NDurationDurationObservation\n\t\nend note\n\t"
+				+ "NDurationDurationObservation..A\n"
+				+ "class A {\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void ClassAssociationTest() {
 		Diagram d = getDiagram(diagrams, "ClassAssociation");
 		
-		assertEquals("@startuml\r\ntitle ClassAssociation\r\n"
-				+ "class A {\r\n}\r\nclass B {\r\n}\r\nB \""
-				+ "b  *\" <-->  \"a  0..1\" A : label\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle ClassAssociation\n"
+				+ "class A {\n}\nclass B {\n}\nB \""
+				+ "b  *\" <-->  \"a  0..1\" A : label\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void GeneralizationTest() {
 		Diagram d = getDiagram(diagrams, "Generalization");
 		
-		assertEquals("@startuml\r\ntitle Generalization\r\n"
-				+ "abstract class A {\r\n}\r\nclass B {\r\n}\r\n"
-				+ "A <|-- B\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Generalization\n"
+				+ "abstract class A {\n}\nclass B {\n}\n"
+				+ "A <|-- B\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void RealizationTest() {
 		Diagram d = getDiagram(diagrams, "Realization");
 		
-		assertEquals("@startuml\r\ntitle Realization\r\n"
-				+ "class B {\r\n}\r\ninterface A {\r\n}\r\n"
-				+ "A <|-- B\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Realization\n"
+				+ "class B {\n}\ninterface A {\n}\n"
+				+ "A <|-- B\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void SharedAssociationTest() {
 		Diagram d = getDiagram(diagrams, "SharedAssociation");
 		
-		assertEquals("@startuml\r\ntitle SharedAssociation\r\n"
-				+ "class A {\r\n}\r\nclass B {\r\n}\r\nclass C {\r\n}\r\n"
-				+ "class D {\r\n}\r\nA \"a  1..*\" o-->  \"b 1\" B \r\nD \""
-				+ "d  1..*\" <--o  \"c 1\" C \r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle SharedAssociation\n"
+				+ "class A {\n}\nclass B {\n}\nclass C {\n}\n"
+				+ "class D {\n}\nA \"a  1..*\" o-->  \"b 1\" B \nD \""
+				+ "d  1..*\" <--o  \"c 1\" C \n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void CompositeTest() {
 		Diagram d = getDiagram(diagrams, "Composite");
 		
-		assertEquals("@startuml\r\ntitle Composite\r\nclass A {\r\n}\r\n"
-				+ "class B {\r\n}\r\nclass C {\r\n}\r\nclass D {\r\n}\r\n"
-				+ "B \"b  *\" <--*  \"a  *\" A \r\nD \""
-				+ "d 1\" *-->  \"c 1\" C \r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle Composite\nclass A {\n}\n"
+				+ "class B {\n}\nclass C {\n}\nclass D {\n}\n"
+				+ "B \"b  *\" <--*  \"a  *\" A \nD \""
+				+ "d 1\" *-->  \"c 1\" C \n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void InstanceSpecificationTwoClassifierTest() {
 		Diagram d = getDiagram(diagrams, "InstanceSpecificationTwoClassifier");
 		
-		assertEquals("@startuml\r\ntitle InstanceSpecificationTwoClassifier\r\n"
-				+ "class \"InstanceSpecification:A,B\r\n\" << (i,pink) >> "
-				+ "{\r\n}\r\nclass A {\r\n}\r\nclass B {\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle InstanceSpecificationTwoClassifier\n"
+				+ "class \"InstanceSpecification:A,B\n\" << (i,pink) >> "
+				+ "{\n}\nclass A {\n}\nclass B {\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void ClassAttributeStaticOperationAbstractStaticTest() {
 		Diagram d = getDiagram(diagrams, "ClassAttributeStaticOperationAbstractStatic");
 		
-		assertEquals("@startuml\r\ntitle ClassAttributeStaticOperationAbstractStatic\r\n"
-				+ "class A {\r\n\t{static}\r\n\t+Attribute1\r\n\t{abstract}\r\n\t"
-				+ "+Operation1()\r\n\t{static}\r\n\t+Operation2()\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle ClassAttributeStaticOperationAbstractStatic\n"
+				+ "class A {\n\t{static}\n\t+Attribute1\n\t{abstract}\n\t"
+				+ "+Operation1()\n\t{static}\n\t+Operation2()\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void ClassVisibilityTest() {
 		Diagram d = getDiagram(diagrams, "ClassVisibility");		
 		
-		assertEquals("@startuml\r\ntitle ClassVisibility\r\nclass A {\r\n\t"
-				+ "+Attribute1\r\n\t-Attribute2\r\n\t#Attribute3\r\n\t~Attribute4\r\n}\r\n"
-				+ "@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle ClassVisibility\nclass A {\n\t"
+				+ "+Attribute1\n\t-Attribute2\n\t#Attribute3\n\t~Attribute4\n}\n"
+				+ "@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void ClassOperationReturnTest() {
 		Diagram d = getDiagram(diagrams, "ClassOperationReturn");
 		
-		assertEquals("@startuml\r\ntitle ClassOperationReturn\r\n"
-				+ "class A {\r\n\t+Operation1(): String\r\n\t"
-				+ "+Operation2(): String[]\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle ClassOperationReturn\n"
+				+ "class A {\n\t+Operation1(): String\n\t"
+				+ "+Operation2(): String[]\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test
 	public void ClassOperationParameterTest() {
 		Diagram d = getDiagram(diagrams, "ClassOperationParameter");
 		
-		assertEquals("@startuml\r\ntitle ClassOperationParameter\r\n"
-				+ "class A {\r\n\t+Attribute1\r\n\t"
-				+ "+Operation1(: String,: String)\r\n\t"
-				+ "+Operation2(: String[])\r\n}\r\n@enduml\r\n", 
-				generator.compileClassDiagram(d).toString());
+		assertEquals("@startuml\ntitle ClassOperationParameter\n"
+				+ "class A {\n\t+Attribute1\n\t"
+				+ "+Operation1(: String,: String)\n\t"
+				+ "+Operation2(: String[])\n}\n@enduml\n", 
+				generator.compileClassDiagram(d).toString().replace("\r", ""));
 	}
 	
 	@Test 
