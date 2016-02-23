4eccf4cf5ea757cdda3e975dcb7a37348cbf959d
Stephan Seifermann <seiferma@users.noreply.github.com>
2016-02-23 17:57:16 +0100

    Fixed build script.

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false"]; then
+if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+	echo mvn clean deploy
 	mvn clean deploy
 else
+	echo mvn clean install
 	mvn clean install
 fi
