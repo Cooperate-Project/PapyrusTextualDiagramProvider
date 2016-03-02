a907e504e2f9748847d47a77f877a6aaf1c3bc6c
Stephan Seifermann <seiferma@users.noreply.github.com>
2016-03-02 17:18:39 +0100

    Made sonar analysis more stable

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-	echo mvn clean deploy
+	echo mvn $1 clean deploy sonar:sonar
 	mvn clean deploy
+elif [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+	echo mvn $1 -Dsonar.branch="$TRAVIS_BRANCH" clean verify sonar:sonar
+	mvn clean verify
 else
-	echo mvn clean install
-	mvn clean install
+	echo mvn $1 clean verify
+	mvn clean verify
 fi
+
+
